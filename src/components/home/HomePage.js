b6ee43d4a1493345500293afd61e4bb6fd31b317
kwiecien <k.d.kwiecinski@gmail.com>
2019-01-11 22:16:53 +0100

    fix typo and display home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,7 +6,7 @@ class HomePage extends React.Component {
     return (
       <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
-        <p>React, Reduc and React Router in ES6</p>
+        <p>React, Redux and React Router in ES6</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
       </div>
     );
