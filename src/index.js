578ff68842fc333e27bbf53c66031fa189a0a95d
kwiecien <k.d.kwiecinski@gmail.com>
2019-01-14 21:17:08 +0100

    create author reducer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import React from 'react';
 import {render} from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
-import {Router, browserHistory} from "react-router";
+import {browserHistory, Router} from "react-router";
 import routes from './routes';
 import {loadCourses} from "./actions/courseActions";
 import './styles/styles.css'; // Webpack can import CSS
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {loadAuthors} from "./actions/authorActions";
 
 const store = configureStore();
+store.dispatch(loadAuthors());
 store.dispatch(loadCourses());
 
 render(
